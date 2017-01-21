f5e12c806116b13ac2d6f23982d10779548006a6
subvertnormality <andy@subvertnormality.com>
2017-01-21 16:07:45 +0000

    Adding missing files

--- launch.bat
+++ launch.bat
@@ -1,2 +1,4 @@
+docker run -d -p 1935:1935 --name nginx-rtmp tiangolo/nginx-rtmp
+docker start nginx-rtmp
 START "" ngrok tcp -region eu --remote-addr 1.tcp.eu.ngrok.io:20187 50051
 python control.py
